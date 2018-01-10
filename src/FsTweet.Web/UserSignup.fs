4132f94aaefd54cc4b31bfbe290e0dff352907f0
landon bass <landon@labtechnologygroup.com>
2018-01-10 13:06:54 -0500

    implemented suave experimental form for user signup

--- src/FsTweet.Web/UserSignup.fs
+++ src/FsTweet.Web/UserSignup.fs
@@ -1,26 +1,32 @@
 namespace UserSignup
 module Suave =
     open Suave
-    open Suave.Filters
-    open Suave.Operators
     open Suave.DotLiquid
+    open Suave.Filters
+    open Suave.Form
+    open Suave.Operators
 
     type UserSignupViewModel = {
         Username : string
-        Email : string
-        Password: string
-        Error : string option
+        Email    : string
+        Password : string
+        Error    : string option
     } 
     let emptyUserSignupViewModel = {
         Username = ""
-        Email = ""
+        Email    = ""
         Password = ""
-        Error = None
+        Error    = None
     }
 
     let handleUserSignup ctx = async {
-        printfn "%A" ctx.request.form
-        return! Redirection.FOUND "/signup" ctx
+        match bindEmptyForm ctx.request with
+        | Choice1Of2 (userSignupViewModel : UserSignupViewModel) ->
+            printfn "%A" userSignupViewModel
+            return! Redirection.FOUND "/signup" ctx
+        | Choice2Of2 err ->
+            let viewModel = {emptyUserSignupViewModel with Error = Some err}
+            return! page "user/signup.liquid" viewModel ctx
     }
 
     let webPart () =
